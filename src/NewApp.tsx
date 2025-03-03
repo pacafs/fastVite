dfee539226a0ffb558aba0e61a295fdfb7749c76
Pedro Fernandes da Silva <paca.fs@gmail.com>
2025-03-03 20:55:49 +0000

    Enhance NewApp with dynamic ListGroup components for countries and animals

--- src/NewApp.tsx
+++ src/NewApp.tsx
@@ -1,9 +1,45 @@
 import ListGroup from "./components/ListGroup";
 
 export default function NewApp() {
+
+    const items = [
+        "United States",
+        "Canada",
+        "United Kingdom",
+        "Australia",
+        "Germany",
+        "France",
+        "Italy",
+        "Spain",
+        "Japan",
+        "Brazil",
+    ];
+
+    const animals = [
+        "Lion",
+        "Tiger",
+        "Elephant",
+        "Giraffe",
+        "Zebra",
+        "Kangaroo",
+        "Panda",
+        "Bear",
+        "Wolf",
+        "Fox",
+    ];
+
+    // Function to log the selected item   
+    const console_log_item = (item: string) => {
+        console.log(item);
+    }
+
     return (
         <div className="container mx-auto">
-            <ListGroup />
+            <h1 className="text-6xl font-bold text-center mt-10">
+                Lists
+            </h1>
+            <ListGroup items={items}   heading="Countries" onSelectItem={console_log_item} />
+            <ListGroup items={animals} heading="Animals" onSelectItem={console_log_item} />
         </div>
     )
 }
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,43 +1,37 @@
-function ListGroup() {
+import { useState } from "react";
 
-    const items = [
-        { id: 1, name: "United States" },
-        { id: 2, name: "Canada" },
-        { id: 3, name: "United Kingdom" },
-        { id: 4, name: "Australia" },
-        { id: 5, name: "Germany" },
-        { id: 6, name: "France" },
-        { id: 7, name: "Italy" },
-        { id: 8, name: "Spain" },
-        { id: 9, name: "Japan" },
-        { id: 10, name: "Brazil" },
-    ]
-    
-    const handle_click = (event: React.MouseEvent<HTMLLIElement>) => {
-        console.log(event.currentTarget);
-    }
+// Props => { items: [], heading: string }
+interface ListGroupProps {
+    items: string[];
+    heading: string;
+    //(item: string) => void
+    onSelectItem: (item: string) => void;
+}
 
-    const items_list = items.map((item) => {
+function ListGroup({ items, heading, onSelectItem }: ListGroupProps) {
+
+    const [selectedIndex, setSelectedIndex] = useState(-1);
+
+    const items_list = items.map((item, index) => {
         return <li 
-                  className="list-row" 
-                  key={item.id} 
-                  onClick={handle_click}>
-                    {item.name}
+                  className={`list-row ${index === selectedIndex && 'bg-docs-bg text-docs-title'}`}
+                  key={index} 
+                  onClick={() => {
+                    setSelectedIndex(index);
+                    onSelectItem(item);
+                  }}>
+                    {item}
                 </li>
     })
-    
-    const get_message = () => {
-        return items.length === 0 && <p>No items found</p>;
-    }
-
 
     return (
         <div className="bg-base-100 rounded-box shadow-md text-center mt-10">
-        <h1 className="text-6xl font-bold">List</h1>
-        <p>{get_message()}</p>
-        <ul className="list bg-base-100 rounded-box shadow-md">
-            {items_list}
-        </ul>
+            <div className="mb-4">
+                <h2 className="text-3xl font-bold">{heading}</h2>
+            </div>
+            <ul className="list bg-base-100 rounded-box shadow-md">
+                {items_list}
+            </ul>
         </div>
     )
 }
